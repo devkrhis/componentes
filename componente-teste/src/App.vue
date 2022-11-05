ba5f5fd65a484e40383a1dc2053544232facb8a7
krhistopher <krhiskk@gmail.com>
2022-11-05 19:04:31 -0300

    componente que falhou

---
+++ componente-teste/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1> Calculadora </h1>
+</template>
+
+<script>
+import Contador from './components/Contador.vue'
+
+
+export default {
+  name: "App",
+  components: {
+    Contador
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ componente-teste/src/components/Contador.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="contador">
+        <h1> Operacoes Matematicas </h1>
+        <span> {{ contador }}</span>
+        <button type="button" @click="Soma()"> Somar +1 </button>
+        <button type="button" @click="Diminuicao()"> Diminuir -1</button>
+        <button type="button" @click="Multiplicacao()"> Multiplicacao *</button>
+        <button type="button" @click="Divisor()"> Divisao /</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Contador',
+    data(){
+        return{
+            contador: 0
+        }
+    },
+    methods:{
+        Soma(){
+            this.contador++
+        },
+        Diminuicao(){
+            this.contador--
+        },
+        Multiplicacao(){
+            this.contador = this.contador * this.contador
+        },
+        Divisor(){
+            this.contador = this.contador / this.contador
+        },
+        Zerador(){
+            this.contador = 0
+        }
+    }
+}
+
+</script>
+
+<style>
+</style>
